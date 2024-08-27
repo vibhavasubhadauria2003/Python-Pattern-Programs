b8a27fe3d217654c6ec17bd9f1d5797198420cb6
Vibhavasu Bhadauria <vibhavasudsvv2003@gmail.com>
2024-08-27 14:03:07 +0530

    StarPattern5

--- NumPattern6.py
+++ NumPattern6.py
@@ -14,7 +14,6 @@ for i in range(1,n+1,1):
     print(" "*(n-i),end="")
     for j in range(1,i+1,1):
         print(j,end="")
-        k=j
     for j in range(i-1,0,-1):
         print(j,end="")
     print()
@@ -22,7 +21,6 @@ for i in range(1,n,1):
     print(" "*i,end="")
     for j in range(1,n+1-i,1):
         print(j,end="")
-        k=j
     for j in range(n-i-1,0,-1):
         print(j,end="")
     print()
